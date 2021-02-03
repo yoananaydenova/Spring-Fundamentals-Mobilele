c6597679d64ed2349cdf8f27131448bd0d5a858f
yoananaydenova <y.naydenova@gmail.com>
2021-02-04 00:56:06 +0200

    Next step: starting the validation.

--- src/main/java/bg/softuni/mobilele/models/services/UserLoginServiceModel.java
+++ src/main/java/bg/softuni/mobilele/models/services/UserLoginServiceModel.java
@@ -1,10 +1,15 @@
 package bg.softuni.mobilele.models.services;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class UserLoginServiceModel {
 
     private String username;
     private String password;
 
+    @NotNull
+    @Size(min=2)
     public String getUsername() {
         return username;
     }
@@ -13,6 +18,8 @@ public class UserLoginServiceModel {
         this.username = username;
     }
 
+    @NotNull
+    @Size(min=3)
     public String getPassword() {
         return password;
     }
